48c9d9daff788ecc74c72f3d5d644e995baf2ac7
Philippe <philippe.idalgoprestes@gmail.com>
2023-11-13 21:08:37 -0300

    arrange function created

--- src/functions/helpFunctions/array.js
+++ src/functions/helpFunctions/array.js
@@ -20,3 +20,36 @@ export function verifyItem(array, item, path){
         }
     }
 }
+
+export function arrange(array){
+    let temp = [], newArray = [];
+    
+    for(let index = 0; index < array.length; index++){
+        let elem = array[index];
+        if(newArray.length == 0){
+            newArray.push(elem);
+        }
+        else{
+            for(let naIndex = 0; naIndex < newArray.length; naIndex++){
+                let elem2 = newArray[naIndex];
+                if(elem2.date.year > elem.date.year || (elem2.date.year == elem.date.year && elem2.date.month > elem.date.month) || (elem2.date.year == elem.date.year && elem2.date.month == elem.date.month && elem2.date.day > elem.date.day)){
+                    for(let naIndex2 = 0; naIndex2 < naIndex; naIndex2++){
+                        temp.push(newArray[naIndex2])
+                    }
+                    temp.push(elem)
+                    for(let naIndex3 = naIndex; naIndex3 < newArray.length; naIndex3++){
+                        temp.push(newArray[naIndex3])
+                    }
+                    newArray = temp;
+                    break
+                }
+                else if(naIndex == newArray.length - 1){
+                    newArray.push(elem)
+                    break
+                }
+            }
+        }
+    }
+
+    return newArray;
+}
--- src/functions/homeFunctions.js
+++ src/functions/homeFunctions.js
@@ -1,8 +1,8 @@
 import { add0, returnR$ } from "./helpFunctions/numberStringify.js"
-import { verifyItem } from "./helpFunctions/array.js"
+import { verifyItem, arrange } from "./helpFunctions/array.js"
 
 export default function getHome(req, res){
-    let a = {userLog: {userName: "Philippe", password: "123456789", userSince: {day: 7, month: 9, year: 2023}},
+    let a = {userLog: {userName: "Philippe", email: "philippe.idalgoprestes@gmail.com", password: "123456789", userSince: {day: 7, month: 9, year: 2023}},
             categories: ["Mensal", "Crédito"],
             transactions: [{name: "Teste", value: 2, date: {day: 1, month: 10, year: 2023}, categories: ["Mensal"]},
                         {name: "UCI", value: -28, date: {day: 1, month: 11, year: 2023}, categories: ["Mensal"]},
@@ -22,35 +22,70 @@ export default function getHome(req, res){
     let infoBasArray = [];
     let infoDetArray = [];
 
-    // infoDet
-    for(let i = 0; i < a.categories.length; i++){
-        let categoryName = a.categories[i];
+    for(let catIndex = 0; catIndex < a.categories.length; catIndex++){
+        let categoryName = a.categories[catIndex];
         let categoryTransactions = [];
         let categoryTransactionsTotal = 0;
         let categoryMonthResult = [];
+        let monthEarningsExpenses = [];
 
-        // Separa as transações desse mês detalhadamente e também prepara o categoryMonthResult
-        for(let j = 0; j < a.transactions.length; j++){
-            let trans = a.transactions[j];
-
+        for(let transIndex = 0; transIndex < a.transactions.length; transIndex++){
+            let trans = a.transactions[transIndex];
+            
+            // Adiciona a transação caso seja dessa categoria e desse mês
             if(trans.date.year == nowYear && trans.date.month == nowMonth && verifyItem(trans.categories, categoryName, null)){
                 let transName = add0(trans.date.day.toString()) + "/" + add0(trans.date.month.toString()) + "/" + trans.date.year.toString() + " - " + trans.name;
                 let [transValue, transType] = returnR$(trans.value);
                 categoryTransactionsTotal += trans.value;
                 categoryTransactions.push({name: transName, value: transValue, type: transType});
             }
-            else if(((trans.date.year == nowYear) || (trans.date.year == nowYear - 1 && trans.date.month >= nowMonth)) && verifyItem(trans.categories, categoryName, null)){
+
+            // Adiciona o resultado da transação para a array de mes e resultado
+            if(((trans.date.year == nowYear) || (trans.date.year == nowYear - 1 && trans.date.month >= nowMonth)) && verifyItem(trans.categories, categoryName, null)){
                 if(categoryMonthResult.length == 0){
                     categoryMonthResult.push({date: {month: trans.date.month, year: trans.date.year}, result: trans.value})
                 }
                 else{
-                    for(let k = 0; k < categoryMonthResult.length; k++){
-                        if(categoryMonthResult[k].date.year == trans.date.year && categoryMonthResult[k].date.month == trans.date.month){
-                            categoryMonthResult[k].result += trans.value;
+                    for(let cmrIndex = 0; cmrIndex < categoryMonthResult.length; cmrIndex++){
+                        if(categoryMonthResult[cmrIndex].date.year == trans.date.year && categoryMonthResult[cmrIndex].date.month == trans.date.month){
+                            categoryMonthResult[cmrIndex].result += trans.value;
                             break;
                         }
-                        else if(k == categoryMonthResult.length - 1){
+                        else if(cmrIndex == categoryMonthResult.length - 1){
                             categoryMonthResult.push({date: {month: trans.date.month, year: trans.date.year}, result: trans.value});
+                        }
+                    }
+                }
+            }
+
+            // Adiciona os ganhos e perdas de cada mês para criar o infoBasArray
+            if(categoryName == "Mensal"){
+                if(monthEarningsExpenses.length == 0){
+                    if(trans.value < 0){
+                        monthEarningsExpenses.push({date: {month: trans.date.month, year: trans.date.year}, earnings: 0, expenses: trans.value})
+                    }
+                    else{
+                        monthEarningsExpenses.push({date: {month: trans.date.month, year: trans.date.year}, earnings: trans.value, expenses: 0})
+                    }
+                }
+                else{
+                    for(let meeIndex = 0; meeIndex < monthEarningsExpenses.length; meeIndex++){
+                        if(categoryMonthResult[meeIndex].date.year == trans.date.year && categoryMonthResult[meeIndex].date.month == trans.date.month){
+                            if(trans.value < 0){
+                                categoryMonthResult[meeIndex].expenses += trans.value;
+                            }
+                            else{
+                                categoryMonthResult[meeIndex].earnings += trans.value;
+                            }
+                            break;
+                        }
+                        else if(meeIndex == categoryMonthResult.length - 1){
+                            if(trans.value < 0){
+                                monthEarningsExpenses.push({date: {month: trans.date.month, year: trans.date.year}, earnings: 0, expenses: trans.value})
+                            }
+                            else{
+                                monthEarningsExpenses.push({date: {month: trans.date.month, year: trans.date.year}, earnings: trans.value, expenses: 0})
+                            }
                         }
                     }
                 }
@@ -61,34 +96,45 @@ export default function getHome(req, res){
         let [value, type] = returnR$(categoryTransactionsTotal);
         let result = {value: value, type: type};
 
-        categoryMonthResult.push({date: {month: nowMonth, year: nowYear}, result: categoryTransactionsTotal})
-
-        //graph
-        // completa com os meses faltantes e verifica o máximo e o mínimo
-        let graphMax = -999999999999999;
-        let graphMin = 999999999999999;
+        let graphMax = 0;
+        let graphMin = 0;
+        // Completa a array de meses
         if(categoryMonthResult.length != 13){
-            for(let m = nowMonth; m <= 12; m++){
-                for(let n = 0; n < categoryMonthResult.length; n++){
-                    if(categoryMonthResult[n].date.month == m && categoryMonthResult[n].date.year == nowYear - 1){
+            let nowMonthLastYearFound = 0;
+            let nowMonthThisYearFound = 0;
+            for(let testMonth = 1; testMonth <= 12; testMonth++){
+                for(let cmrIndex2 = 0; cmrIndex2 < categoryMonthResult.length; cmrIndex2++){
+                    if((testMonth > nowMonth && categoryMonthResult[cmrIndex2].date.month == testMonth && categoryMonthResult[cmrIndex2].date.year == nowYear - 1) || 
+                    (testMonth < nowMonth && categoryMonthResult[cmrIndex2].date.month == testMonth && categoryMonthResult[cmrIndex2].date.year == nowYear)){
                         break;
                     }
-                    else if(n == categoryMonthResult.length - 1){
-                        categoryMonthResult.push({date: {month: m, year: nowYear - 1}, result: 0})
-                    }
-                }
-            }
-            for(let o = 1; o < nowMonth; o++){
-                for(let p = 0; p < categoryMonthResult.length; p++){
-                    if(categoryMonthResult[p].date.month == o && categoryMonthResult[p].date.year == nowYear){
+                    else if(testMonth == nowMonth && categoryMonthResult[cmrIndex2].date.month == nowMonth && categoryMonthResult[cmrIndex2].date.year == nowYear){
+                        nowMonthThisYearFound = 1;
                         break;
                     }
-                    else if(p == categoryMonthResult.length - 1){
-                        categoryMonthResult.push({date: {month: o, year: nowYear}, result: 0})
+                    else if(testMonth == nowMonth && categoryMonthResult[cmrIndex2].date.month == nowMonth && categoryMonthResult[cmrIndex2].date.year == nowYear - 1){
+                        nowMonthLastYearFound = 1;
+                        break;
+                    }
+                    else if(cmrIndex2 == categoryMonthResult.length - 1){
+                        let objYear;
+                        if((testMonth > nowMonth) || (testMonth == nowMonth && nowMonthLastYearFound == 0 && nowMonthThisYearFound == 1)){
+                            objYear = nowYear - 1;
+                        }
+                        else if ((testMonth < nowMonth) || (testMonth == nowMonth && nowMonthLastYearFound == 1 && nowMonthThisYearFound == 0)){
+                            objYear = nowYear;
+                        }
+                        else if (testMonth == nowMonth && nowMonthLastYearFound == 0 && nowMonthThisYearFound == 0){
+                            objYear = nowYear;
+                            categoryMonthResult.push({date: {month: testMonth, year: objYear - 1}, result: 0})
+                        }
+                        categoryMonthResult.push({date: {month: testMonth, year: objYear}, result: 0})
                     }
                 }
             }
         }
+
+        // Encontra o máximo e mínimo para o gráfico
         for(let a = 0; a < categoryMonthResult.length; a++){
             if(categoryMonthResult[a].result > graphMax){
                 graphMax = categoryMonthResult[a].result
@@ -97,73 +143,25 @@ export default function getHome(req, res){
                 graphMin = categoryMonthResult[a].result
             }
         }
+        
+        // Estabelece uma distância mínima entre eles
+        if(graphMax - graphMin < 100){
+            graphMax += 50;
+            graphMin -+ 50;
+        }
+
+        console.log(categoryMonthResult);
 
         let graphBody = [];
         //gera as porcentagens no gráfico
         let cx = 5;
-        for(let b = nowMonth; b <= 12; b++){
-            cx += 7.5;
-            for(let q = 0; q < categoryMonthResult.length; q++){
-                if(categoryMonthResult[q].date.month == b && categoryMonthResult[q].date.year == nowYear - 1){
-                    let cat = categoryMonthResult[q]
-                    let title
-                    let reference
-                    if (graphMax >= 0){
-                        reference = graphMax - graphMin
-                    }
-                    else{
-                        reference = graphMin - graphMax
-                    }
-                    let porcentage
-                    if(graphMin < 0){
-                        if(cat.result >= 0){
-                            porcentage = Math.round(((cat.result - graphMin)/reference)*90 + 5).toString() + "%";
-                            title = add0(cat.date.month.toString()) + "/" + cat.date.year.toString() + " - " + returnR$(cat.result)[0];
-                        }
-                        else{
-                            porcentage = Math.round(((graphMin - cat.result)/reference)*90 + 5).toString() + "%";
-                            title = add0(cat.date.month.toString()) + "/" + cat.date.year.toString() + " - -" + returnR$(cat.result)[0];
-                        }
-                    }
-                    else{
-                        porcentage = Math.round(((cat.result + graphMin)/reference)*90 + 5).toString() + "%";
-                        title = add0(cat.date.month.toString()) + "/" + cat.date.year.toString() + " - " + returnR$(cat.result)[0];
-                    }
-                    
-                    graphBody.push({porcentage: porcentage, title: title, cx: cx.toString() + "%"})
-                    break;
-                }
-            }
-        }
-        for(let d = 1; d <= nowMonth; d++){
-            cx += 7.5;
-            for(let w = 0; w < categoryMonthResult.length; w++){
-                if(categoryMonthResult[w].date.month == d && categoryMonthResult[w].date.year == nowYear){
-                    let cat = categoryMonthResult[w]
-                    let title
-                    let reference
-                    if (graphMax >= 0){
-                        reference = graphMax - graphMin
-                    }
-                    else{
-                        reference = graphMin - graphMax
-                    }
-                    let porcentage
-                    if(graphMin < 0){
-                        if(cat.result >= 0){
-                            porcentage = Math.round(((cat.result - graphMin)/reference)*90 + 5).toString() + "%";
-                            title = add0(cat.date.month.toString()) + "/" + cat.date.year.toString() + " - " + returnR$(cat.result)[0];
-                        }
-                        else{
-                            porcentage = Math.round(((graphMin - cat.result)/reference)*90 + 5).toString() + "%";
-                            title = add0(cat.date.month.toString()) + "/" + cat.date.year.toString() + " - -" + returnR$(cat.result)[0];
-                        }
-                    }
-                    else{
-                        porcentage = Math.round(((cat.result - graphMin)/reference)*90 + 5).toString() + "%";
-                        title = add0(cat.date.month.toString()) + "/" + cat.date.year.toString() + " - " + returnR$(cat.result)[0];
-                    }
-                    
+        for(let testMonth2 = nowMonth; testMonth2 <= 12; testMonth2++){
+            for(let cmrIndex3 = 0; cmrIndex3 < categoryMonthResult.length; cmrIndex3++){
+                let cat = categoryMonthResult[cmrIndex3]
+                let title
+                let reference = graphMax - graphMin;
+                if(categoryMonthResult[cmrIndex3].date.month == testMonth2 && categoryMonthResult[cmrIndex3].date.year == nowYear - 1){
+
                     graphBody.push({porcentage: porcentage, title: title, cx: cx.toString() + "%"})
                     break;
                 }
