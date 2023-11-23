7c881c7c80f1ae20f47faf12d466d11f8c2d17c2
Philippe <philippe.idalgoprestes@gmail.com>
2023-11-23 12:33:41 -0300

    signup host

--- src/functions/startFunctions.js
+++ src/functions/startFunctions.js
@@ -1,11 +1,15 @@
 export function postLogin(req, res){
-    let userEmail = req.email
-    let userPassword = req.password
+    let userEmail = req.body.email
+    let userPassword = req.body.password
 
-    res.status(402).end()
+    res.status(200).end()
     // 401 for wrong pass ans 402 for wrong user
 }
 
 export function postSignUp(req, res){
-    
+    let userName = req.body.name
+    let userEmail = req.body.email
+    let userPassword = req.body.password
+    res.status(200).send(userName).end()
+    //409 conflict
 }
